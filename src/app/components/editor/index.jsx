4a3a3868b4165fc4bc3b07f9feb24ef1ab408870
Jonathan Reilly <jonathan@wistia.com>
2016-10-29 15:58:28 -0400

    Set up decay on keyup in editor.

--- src/app/components/editor/index.jsx
+++ src/app/components/editor/index.jsx
@@ -6,6 +6,7 @@ import {connect}              from 'react-redux';
 import accessors              from './accessors';
 import actions                from './actions';
 import styles                 from './styles';
+import Timer                  from '../timer'
 
 function mapStateToProps (state) {
   return {
@@ -21,22 +22,57 @@ function mapDispatchToProps (dispatch) {
 
 
 class Editor extends Component {
+  constructor (props) {
+    super()
+    this.state = {
+      isDecayable: false,
+      decayDuration: 5,
+      isDecaying: false,
+      resetDecay: false
+    }
+  }
+
   onTextChange (event) {
     this.props.setPassage(event.currentTarget.value)
   }
 
+  resetTimer () {
+    this.setState({resetDecay: true, isDecaying: false})
+  }
+
+  startTimer () {
+    if (this.state.isDecayable) {
+      this.setState({isDecaying: true})
+    }
+  }
+
+  clearPassage () {
+    this.setState({isDecaying: false})
+    this.props.setPassage('')
+  }
+
   render () {
-    const { life } = this.props
-    return(
-      <div>
-        <textarea
-          className={styles.editor}
-          onChange={this.onTextChange.bind(this)}
-          placeholder='Enter your passage'
-          style={{opacity: life * 0.01}}
-          value={this.props.passage}
-          >
-        </textarea>
+    const { decayDuration, isDecaying, resetDecay } = this.state
+    return (
+      <div className={styles.editorContainer}>
+        <Timer duration={decayDuration}
+               isRunning={isDecaying}
+               shouldReset={resetDecay}
+               onStop={this.clearPassage.bind(this)}
+               onReset={() => this.setState({resetDecay: false})}>
+          {(seconds, percent) =>
+            <textarea
+              className={styles.editor}
+              onChange={this.onTextChange.bind(this)}
+              placeholder='Enter your passage'
+              style={{opacity: percent == 100 ? 1 : (100 - percent) * 0.01}}
+              onKeyDown={this.resetTimer.bind(this)}
+              onKeyUp={this.startTimer.bind(this)}
+              value={this.props.passage}
+              >
+            </textarea>
+          }
+        </Timer>
       </div>
     )
   }
--- src/app/main.jsx
+++ src/app/main.jsx
@@ -10,51 +10,45 @@ class Main extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      decayDuration: 5,
-      isDecaying: false,
-      shouldReset: false
+      sessionClock: {
+        sessionDuration: 60,
+        isInSession: false,
+        resetSession: false
+      },
+      decayClock: {
+        decayDuration: 5,
+        isDecaying: false,
+        resetDecay: false
+      }
     }
   }
-
-  startCountDown () {
-    this.setState({isDecaying: true})
-  }
-
-  stopCountDown () {
-    this.setState({isDecaying: false})
-  }
-
-  triggerShouldReset () {
-    this.setState({shouldReset: true})
-  }
-
-  clearShouldReset () {
-    this.setState({shouldReset: false})
-  }
+  // Session Timer
+  startSession ()     {this.setState({sessionClock: {isInSession: true}})}
+  stopSession ()      {this.setState({sessionClock: {isInSession: false}})}
+  triggerShouldResetSession () {this.setState({sessionClock: {resetSession: true}})}
+  clearShouldResetSession ()   {this.setState({sessionClock: {resetSession: false}})}
 
   render () {
-    const { decayDuration, isDecaying, shouldReset } = this.state
+    const { sessionDuration, isInSession, resetSession } = this.state.sessionClock
+    const { decayDuration, isDecaying, resetDecay } = this.state.decayClock
     return(
       <Provider store={store}>
         <div>
-          <Timer duration={decayDuration}
-                 isRunning={isDecaying}
-                 shouldReset={shouldReset}
-                 onReset={this.clearShouldReset.bind(this)}
-                 onStop={this.stopCountDown.bind(this)}
-                 >
+          <Timer duration={60}
+                 isRunning={isInSession}
+                 shouldReset={resetSession}
+                 onStop={this.stopSession.bind(this)}
+                 onReset={this.clearShouldResetSession.bind(this)}>
             {(seconds, percent) =>
               <div>
-                <pre>
-                  {JSON.stringify({decayTimer: {percent, seconds}}, null, 1)}
-                </pre>
-                <Editor life={100 - percent} />
+                <Clock seconds={seconds} />
+                <Editor />
               </div>
             }
           </Timer>
-          <button onClick={this.startCountDown.bind(this)}>START</button>
-          <button onClick={this.stopCountDown.bind(this)}>STOP</button>
-          <button onClick={this.triggerShouldReset.bind(this)}>RESET</button>
+          <button onClick={this.startSession.bind(this)}>START</button>
+          <button onClick={this.stopSession.bind(this)}>STOP</button>
+          <button onClick={this.triggerShouldResetSession.bind(this)}>RESET</button>
         </div>
       </Provider>
     )
