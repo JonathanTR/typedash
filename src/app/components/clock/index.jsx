b87b491d3ae5430593640e350f5eb8674e23ff6d
Jonathan Reilly <jonathan@wistia.com>
2016-10-29 16:36:32 -0400

    Add a little styles to the shotclock.

--- src/app/components/clock/index.jsx
+++ src/app/components/clock/index.jsx
@@ -1,6 +1,7 @@
 import React, {Component}     from 'react';
 import ReactDOM               from 'react-dom';
 
+import styles                 from './styles';
 
 class Clock extends Component {
   unitsFromSeconds (timeInSeconds) {
@@ -24,7 +25,7 @@ class Clock extends Component {
   render () {
     const { hours, seconds, minutes } = this.unitsFromSeconds(this.props.seconds)
     return (
-      <div>
+      <div className={styles.shotClock}>
         {hours ? `${this.padZero(hours)}:` : ''}
         {`${this.padZero(minutes)}`}:
         {`${this.padZero(seconds)}`}
