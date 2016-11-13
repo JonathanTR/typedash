a62da942d6258377469eba8c5284c42358e40752
Jonathan Reilly <jonathan@wistia.com>
2016-11-13 10:45:03 -0500

    Fix package json resolution issues.

--- src/app/components/timer/index.test.jsx
+++ src/app/components/timer/index.test.jsx
@@ -1,5 +1,21 @@
-import Timer from './index'
+import React from 'react';
+import Timer from './index';
+import {mount} from 'enzyme';
+import {jsdom} from 'jsdom';
 
-test('true is true', () => {
-  expect(true).toEqual(true)
+global.document = jsdom('<!doctype html><html><body></body></html>');
+global.window = document.defaultView;
+global.navigator = global.window.navigator;
+
+
+it('renders correctly and begins at 0', () => {
+  const timer = mount(
+    <Timer duration={10}>
+      {(seconds, percent) => {
+        return(<div>{seconds}</div>)
+      }}
+    </Timer>
+  )
+
+  expect(timer.text()).toEqual('0')
 })
